ab0f278df79193f7f4fc162158758c259b90ec28
Max Randolph <azzmodious@gmail.com>
2017-07-11 22:11:24 -0400

    changed greetings

--- messages/index.js
+++ messages/index.js
@@ -49,7 +49,7 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 */
 .matches('greeting', (session, arg) =>{
-    session.send('Whats up buddy :)');
+    session.send('Whats up bro slice:) ');
     
 })
 .matches('submit ticket', (session, arg) =>{
